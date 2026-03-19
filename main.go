3619f25d7d208df9bcf07ecaab014757743f1cdc
Urethramancer <github@grimdork.net>
2026-03-19 08:01:32 +0100

    cli: add --version/-v flag (version set via ldflags)

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/grimdork/kush/internal/shell"
 )
 
+// version is populated at build time via -ldflags "-X main.version=..."
+var version = "dev"
+
 func main() {
+	showVersion := flag.Bool("version", false, "print version and exit")
+	shortV := flag.Bool("v", false, "print version and exit (shorthand)")
+	flag.Parse()
+
+	if *showVersion || *shortV {
+		fmt.Println(version)
+		return
+	}
+
 	if err := shell.Run(); err != nil {
 		log.Fatal(err)
 	}
