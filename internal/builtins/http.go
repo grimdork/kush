8cab4777533e3aef334757465cc808fb67a31ec0
Urethramancer <github@grimdork.net>
2026-03-21 23:15:04 +0100

    Isolated shellSplit().

--- internal/builtins/http.go
+++ internal/builtins/http.go
@@ -237,32 +237,3 @@ func (b *Builtins) handleFetch(line string) bool {
 	}
 	return true
 }
-
-// shellSplit splits a command line respecting single and double quotes.
-func shellSplit(line string) []string {
-	var tokens []string
-	var current strings.Builder
-	inSingle := false
-	inDouble := false
-
-	for i := 0; i < len(line); i++ {
-		c := line[i]
-		switch {
-		case c == '\'' && !inDouble:
-			inSingle = !inSingle
-		case c == '"' && !inSingle:
-			inDouble = !inDouble
-		case c == ' ' && !inSingle && !inDouble:
-			if current.Len() > 0 {
-				tokens = append(tokens, current.String())
-				current.Reset()
-			}
-		default:
-			current.WriteByte(c)
-		}
-	}
-	if current.Len() > 0 {
-		tokens = append(tokens, current.String())
-	}
-	return tokens
-}
---
+++ internal/builtins/strings.go
@@ -0,0 +1,32 @@
+package builtins
+
+import "strings"
+
+// shellSplit splits a command line respecting single and double quotes.
+func shellSplit(line string) []string {
+	var tokens []string
+	var current strings.Builder
+	inSingle := false
+	inDouble := false
+
+	for i := 0; i < len(line); i++ {
+		c := line[i]
+		switch {
+		case c == '\'' && !inDouble:
+			inSingle = !inSingle
+		case c == '"' && !inSingle:
+			inDouble = !inDouble
+		case c == ' ' && !inSingle && !inDouble:
+			if current.Len() > 0 {
+				tokens = append(tokens, current.String())
+				current.Reset()
+			}
+		default:
+			current.WriteByte(c)
+		}
+	}
+	if current.Len() > 0 {
+		tokens = append(tokens, current.String())
+	}
+	return tokens
+}
